7cb964670e113e30f8fa169f64985bbf12a27bcc
narayanab16 <narayanab16@gmail.com>
2023-10-23 21:15:06 -0500

    description added

--- src/main/java/com/narayana/bank/app/server/BankApplication.java
+++ src/main/java/com/narayana/bank/app/server/BankApplication.java
@@ -6,8 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * This is a spring boot web reactive microservice to handle bank aplication
- * (customer registration, account number generation, branch, transaction record)
+ * This is a spring boot web reactive microservice to handle bank aplication.
+ * The following activities are performed.
+ *
+ * 1. Customer registration
+ * 2. Add new branches to the system
+ * 3. Account number generation to the customer, and link account number to the specific branch,
+ * 4. Capture all the transaction records link to the specific account
+ * 5. Get the account details
+ * 6. etc.
  *
  * @author : Narayana Basetty
  * @created on : Oct/22/2023
